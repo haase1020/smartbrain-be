3e217da9381f12519b1dc32d4566f539e6bfc6a8
Jon Haase <haase1020@gmail.com>
2020-07-04 13:55:41 -0400

    begin working on endpoints

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send();
+  res.send('this is working');
 });
-
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
